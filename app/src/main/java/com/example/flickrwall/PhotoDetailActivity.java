1cac56bdc3e8f943d934edbd22d464743890ef2f
max <maxkobalt86@gmail.com>
2019-02-23 20:37:06 +0400

    PhotoDetail

--- app/src/main/java/com/example/flickrwall/PhotoDetailActivity.java
+++ app/src/main/java/com/example/flickrwall/PhotoDetailActivity.java
@@ -1,7 +1,13 @@
 package com.example.flickrwall;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class PhotoDetailActivity extends BaseActivity{
 
@@ -11,5 +17,27 @@ public class PhotoDetailActivity extends BaseActivity{
         setContentView(R.layout.activity_photo_detail);
 
         activateToolbar(true);
+
+        Intent intent = getIntent();
+        Photo photo = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
+        if (photo != null) {
+            TextView photoTitle = findViewById(R.id.photo_title);
+            Resources resources = getResources();
+            String text = resources.getString(R.string.photo_tags_text, photo.getTitle());
+            photoTitle.setText(text);
+
+            TextView photoTags = findViewById(R.id.photo_tags);
+            photoTags.setText(resources.getString(R.string.photo_tags_text, photo.getTags()));
+
+            TextView photoAuthor = findViewById(R.id.photo_author);
+            photoAuthor.setText(photo.getAuthor());
+
+            ImageView  photoImage = findViewById(R.id.photo_image);
+
+            Picasso.with(this).load(photo.getLink())
+                    .error(R.drawable.placeholder)
+                    .placeholder(R.drawable.placeholder)
+                    .into(photoImage);
+        }
     }
 }
